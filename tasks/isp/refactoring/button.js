fee5a321c5764134b6c51627d3c1940646682d5e
Artem Kuvaldin <akuv@yandex-team.ru>
2014-04-23 11:47:22 +0600

    refactoring

--- tasks/isp/refactoring/button.js
+++ tasks/isp/refactoring/button.js
@@ -5,56 +5,56 @@ function notImplemented() {
 function AbstractButton(label) {
     this.label = label;
     this.$el = this.render();
-    this.bind();
 }
 AbstractButton.prototype = {
     getLabel: function() {
         return this.label;
     },
-    getIcon: function () {
-        notImplemented();
-    },
-    getUrl: function () {
-        notImplemented();
-    },
-    onClick: function (e) {
-        notImplemented();
-    },
     render: function () {
-        var icon = this.getIcon();
-        var url = this.getUrl();
         var label = this.getLabel();
         var $el = $('<button></button>');
-
-        if (icon) {
-            $el.append($('<img/>').attr('src', icon));
-        }
 
         if (label) {
             $el.append(label);
         }
 
-        if (url) {
-            $el.wrap('<a href="' + url + '"></a>');
-        }
-
         return $el;
+    }
+};
+
+function clickButton () {
+    AbstractButton.call(this, void 0);
+    this.bind();
+}
+
+clickButton.prototype = Object.extend(Object.create(AbstractButton.prototype), {
+    onClick: function (e) {
+        notImplemented();
     },
     bind: function () {
         this.$el.click(this.onClick);
     }
-};
+})
 
 function IconButton(icon) {
     AbstractButton.call(this, void 0);
     this.icon = icon;
 }
+
 IconButton.prototype = Object.extend(Object.create(AbstractButton.prototype), {
     getIcon: function () {
         return this.icon;
     },
-    getUrl: function () {},
-    onClick: function (e) {}
+    render: function () {
+        var $el = AbstractButton.prototype.render.apply(this);
+        var icon = this.getIcon();
+
+        if (icon) {
+            $el.append($('<img/>').attr('src', icon));
+        }
+
+        return $el;
+    }
 });
 
 function AnchorButton(label, url) {
@@ -62,9 +62,17 @@ function AnchorButton(label, url) {
     this.url = url;
 }
 AnchorButton.prototype = Object.extend(Object.create(AbstractButton.prototype), {
-    getIcon: function () {},
     getUrl: function () {
         return this.url;
     },
-    onClick: function (e) {}
+    render: function () {
+        var $el = AbstractButton.prototype.render.apply(this);
+        var url = this.getUrl();
+
+        if (url) {
+            $el.wrap('<a href="' + url + '"></a>');
+        }
+
+        return $el;
+    }
 });
